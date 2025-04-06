9da0a130e722e85c8f5752e605898fd135ccff2c
Priyanshu Singh <sbhola833@gmail.com>
2025-04-06 22:00:10 +0000

    Add serializers for Menu, MenuItem, Order, and OrderItem models

--- apis/storeType/restaurant/serializers.py
+++ apis/storeType/restaurant/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
-from .models import Restaurant
+from .models import Restaurant, Menu, MenuItem, Order, OrderItem
+from django.utils.translation import gettext_lazy as _
+
+
 
 class RestaurantInfoSerializer(serializers.ModelSerializer):
     """
@@ -22,4 +25,59 @@ class RestaurantInfoSerializer(serializers.ModelSerializer):
             'closing_time': {'required': True},
             'is_active': {'required': False}
         }
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Menu model.
+    """
+    class Meta:
+        model = Menu
+        fields = '__all__'
+        read_only_fields = ['created_at', 'updated_at']
+        extra_kwargs = {
+            'restaurant': {'required': True},
+            'category_name': {'required': True}
+        }
         
+class MenuItemSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the MenuItem model.
+    """
+    class Meta:
+        model = MenuItem
+        fields = '__all__'
+        read_only_fields = ['created_at', 'updated_at']
+        extra_kwargs = {
+            'menu': {'required': True},
+            'name': {'required': True},
+            'price': {'required': True}
+        }
+
+class OrderSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Order model.
+    """
+    class Meta:
+        model = Order
+        fields = '__all__'
+        read_only_fields = ['created_at', 'updated_at']
+        extra_kwargs = {
+            'restaurant': {'required': True},
+            'user': {'required': True},
+            'order_status': {'required': True}
+        }
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the OrderItem model.
+    """
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+        read_only_fields = ['created_at', 'updated_at']
+        extra_kwargs = {
+            'order': {'required': True},
+            'item': {'required': True},
+            'quantity': {'required': True}
+        }
